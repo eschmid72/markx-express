1675c4a60f5f21a97f2d3e8ace2b9405ba8914ae
Egon Schmid <eschmid1972@gmail.com>
2015-12-02 13:09:47 +0100

    0.0.1

--- app.js
+++ app.js
@@ -1,27 +1,77 @@
-var express = require('express');
+var fs = require('fs'),
+    path = require('path'),
+    url = require('url'),
+    express = require('express'),
+    marked = require('marked');
+
 var app = express();
 
 app.set('view engine', 'jade');
 
-app.use('/bower_components',  express.static(__dirname + '/bower_components'));
+app.use('/bower_components', express.static(__dirname + '/bower_components'));
 
-app.use(require('express-markdown')({
+marked.setOptions({
+    highlight: function (code) {
+        return require('highlight.js').highlightAuto(code).value;
+    }
+});
 
-  // directory where markdown files are stored
-  // required
-  directory: __dirname + '/pages',
+var expressMarkdown = function (options) {
+    if (!options)
+        throw new Error("Missing options argument");
 
-  // view to use for rendering markdown file
-  // optional
-  // default is undefined, no view
-  view: 'layout',
+    var dir = options.directory,
+        view = options.view,
+        variable = options.variable || 'markdown';
 
-  // name of markdown variable passed in the context when rendering
-  // optional
-  // default 'markdown'
-  variable: 'content'
+    if (!dir)
+        throw new Error('Missing "directory" value in options');
 
+    // clean up path, remove '..'
+    dir = path.resolve(dir);
+
+    return function (req, res, next) {
+        var file = req.url.toString().toLowerCase();
+
+        if(file==='/') {
+            file='index';
+        }
+
+        file = dir + '/' + url.parse(file).pathname;
+        file = path.resolve(file) + '.md';
+
+        // make sure the final path is in our defined directory
+        if (file.substr(0, dir.length) !== dir)
+            return res.send(400);
+
+        fs.exists(file, function (exists) {
+            if (!exists)
+                return next();
+
+            fs.readFile(file, 'utf8', function (err, data) {
+                var context = {};
+                if (err)
+                    return next(err);
+
+                data = marked(data);
+
+                if (view) {
+                    context[variable] = data;
+                    res.render(view, context);
+                }
+                else {
+                    res.send(data);
+                }
+            });
+        });
+    }
+}
+
+app.use(expressMarkdown({
+    directory: __dirname + '/pages',
+    view: 'markdown'
 }));
+
 
 app.listen(5000);
 
