bcf63abb69c2c9e070dcd5508b9c3a7546d7b427
Egon Schmid <eschmid1972@gmail.com>
2015-12-02 10:48:07 +0100

    update

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var app = express();
 
+app.set('view engine', 'jade');
+
 // respond with "hello world" when a GET request is made to the homepage
 app.get('/', function(req, res) {
-  res.send('hello world');
+  res.render('index', { title: 'Hey', message: 'Hello there!'});
 });
 
 app.listen(5000);
