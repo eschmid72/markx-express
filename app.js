f35809e069775099fd82458093d42d8f1c36a21a
„dein <deinemailadresse@whatever.com>
2016-01-01 17:00:51 +0100

    code cleanup

--- app.js
+++ app.js
@@ -1,89 +1,70 @@
 var fs = require('fs'),
-    path = require('path'),
-    url = require('url'),
-    express = require('express'),
-    hljs = require('highlight.js');
+	path = require('path'),
+	url = require('url'),
+	express = require('express'),
+	_ = require('underscore');
+hljs = require('highlight.js');
 
 var md = require('markdown-it')({
-    highlight: function (str, lang) {
-        if (lang && hljs.getLanguage(lang)) {
-            try {
-                console.log(str, lang)
-                return hljs.highlight(lang, str).value;
-            } catch (__) {}
-        }
+	highlight: function (str, lang) {
+		if (lang && hljs.getLanguage(lang)) {
+			try {
+				return hljs.highlight(lang, str).value;
+			} catch (__) {
+			}
+		}
 
-        try {
-            return hljs.highlightAuto(str).value;
-        } catch (__) {}
+		try {
+			return hljs.highlightAuto(str).value;
+		} catch (__) {
+		}
 
-        return ''; // use external default escaping
-    }
+		return ''; // use external default escaping
+	}
 });
 
-var app = express();
-
-app.set('view engine', 'jade');
-
-app.use('/bower_components', express.static(__dirname + '/bower_components'));
-
-var config = fs.existsSync(__dirname+'/config.json') ? require('./config.json') : {};
-
 var expressMarkdown = function (options) {
-    if (!options)
-        throw new Error("Missing options argument");
+	var dir = path.resolve(options.directory);
 
-    var dir = options.directory,
-        view = options.view,
-        variable = options.variable || 'markdown';
+	return function (req, res, next) {
+		var file = req.url.toString().toLowerCase();
 
-    if (!dir)
-        throw new Error('Missing "directory" value in options');
+		if (file === '/') {
+			file = 'index';
+		}
 
-    // clean up path, remove '..'
-    dir = path.resolve(dir);
+		file = dir + '/' + url.parse(file).pathname;
+		file = path.resolve(file) + '.md';
 
-    return function (req, res, next) {
-        var file = req.url.toString().toLowerCase();
+		if (file.substr(0, dir.length) !== dir) {
+			return res.send(400);
+		}
 
-        if(file==='/') {
-            file='index';
-        }
+		fs.exists(file, function (exists) {
+			if (!exists)
+				return next();
 
-        file = dir + '/' + url.parse(file).pathname;
-        file = path.resolve(file) + '.md';
+			fs.readFile(file, 'utf8', function (err, data) {
+				if (err) {
+					return next(err);
+				}
 
-        // make sure the final path is in our defined directory
-        if (file.substr(0, dir.length) !== dir)
-            return res.send(400);
-
-        fs.exists(file, function (exists) {
-            if (!exists)
-                return next();
-
-            fs.readFile(file, 'utf8', function (err, data) {
-                var context = {};
-                if (err)
-                    return next(err);
-
-                var html = md.render(data);
-
-                if (view) {
-                    context[variable] = html;
-                    res.render(view, context);
-                }
-                else {
-                    res.send(data);
-                }
-            });
-        });
-    }
+				res.render('markdown', {markdown: md.render(data)});
+			});
+		});
+	}
 };
 
-app.use(expressMarkdown({
-    directory: config.directory || __dirname + '/sampledoc',
-    view: 'markdown'
-}));
+var config = fs.existsSync(__dirname + '/config.json') ? require('./config.json') : {};
 
-app.listen(config.port || 5000);
+config = _.extend({
+	directory: __dirname + '/samples'
+}, config);
 
+var app = express();
+app.set('view engine', 'jade');
+app.use('/bower_components', express.static(__dirname + '/bower_components'));
+app.use(expressMarkdown(config));
+var port = config.port || 5000;
+app.listen(port);
+console.log('App listening on port '+port)
