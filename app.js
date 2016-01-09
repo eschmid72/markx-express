d546b64ce9380d3de5a0b8a5f0444c9633459384
„dein <deinemailadresse@whatever.com>
2016-01-09 23:55:08 +0100

    code indent 2 space

--- app.js
+++ app.js
@@ -1,65 +1,65 @@
 var fs = require('fs'),
-	path = require('path'),
-	url = require('url'),
-	express = require('express'),
-	_ = require('underscore');
+  path = require('path'),
+  url = require('url'),
+  express = require('express'),
+  _ = require('underscore');
 hljs = require('highlight.js');
 
 var md = require('markdown-it')({
-	highlight: function (str, lang) {
-		if (lang && hljs.getLanguage(lang)) {
-			try {
-				return hljs.highlight(lang, str).value;
-			} catch (__) {
-			}
-		}
+  highlight: function (str, lang) {
+    if (lang && hljs.getLanguage(lang)) {
+      try {
+        return hljs.highlight(lang, str).value;
+      } catch (__) {
+      }
+    }
 
-		try {
-			return hljs.highlightAuto(str).value;
-		} catch (__) {
-		}
+    try {
+      return hljs.highlightAuto(str).value;
+    } catch (__) {
+    }
 
-		return ''; // use external default escaping
-	}
+    return ''; // use external default escaping
+  }
 })
-.use(require('markdown-it-anchor'), {})
-.use(require('markdown-it-toc'));
+  .use(require('markdown-it-anchor'), {})
+  .use(require('markdown-it-toc'));
 
 var expressMarkdown = function (options) {
-	var dir = path.resolve(options.directory);
+  var dir = path.resolve(options.directory);
 
-	return function (req, res, next) {
-		var file = req.url.toString().toLowerCase();
+  return function (req, res, next) {
+    var file = req.url.toString().toLowerCase();
 
-		if (file === '/') {
-			file = 'index';
-		}
+    if (file === '/') {
+      file = 'index';
+    }
 
-		file = dir + '/' + url.parse(file).pathname;
-		file = path.resolve(file) + '.md';
+    file = dir + '/' + url.parse(file).pathname;
+    file = path.resolve(file) + '.md';
 
-		if (file.substr(0, dir.length) !== dir) {
-			return res.send(400);
-		}
+    if (file.substr(0, dir.length) !== dir) {
+      return res.send(400);
+    }
 
-		fs.exists(file, function (exists) {
-			if (!exists)
-				return next();
+    fs.exists(file, function (exists) {
+      if (!exists)
+        return next();
 
-			fs.readFile(file, 'utf8', function (err, data) {
-				if (err) {
-					return next(err);
-				}
-				res.render('markdown', {markdown: md.render(data)});
-			});
-		});
-	}
+      fs.readFile(file, 'utf8', function (err, data) {
+        if (err) {
+          return next(err);
+        }
+        res.render('markdown', {markdown: md.render(data)});
+      });
+    });
+  }
 };
 
 var config = fs.existsSync(__dirname + '/config.json') ? require('./config.json') : {};
 
 config = _.extend({
-	directory: __dirname + '/samples'
+  directory: __dirname + '/samples'
 }, config);
 
 var app = express();
@@ -68,4 +68,4 @@ app.use('/bower_components', express.static(__dirname + '/bower_components'));
 app.use(expressMarkdown(config));
 var port = config.port || 5000;
 app.listen(port);
-console.log('App listening on port '+port)+'.';
+console.log('App listening on port ' + port) + '.';
