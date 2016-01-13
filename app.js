3a1e50b9e6cd4873e68c84868f73716b9b510cfc
Egon Schmid <eschmid1972@gmail.com>
2016-01-13 02:31:31 +0100

    REDO

--- app.js
+++ app.js
@@ -2,17 +2,73 @@ var fs = require('fs'),
   path = require('path'),
   url = require('url'),
   express = require('express'),
-  _ = require('underscore');
+  _ = require('underscore'),
+  hljs = require('highlight.js');
 
+var md = require('markdown-it')({
+  highlight: function (str, lang) {
+    if (lang && hljs.getLanguage(lang)) {
+      try {
+        return hljs.highlight(lang, str).value;
+      } catch (__) {
+      }
+    }
+
+    try {
+      return hljs.highlightAuto(str).value;
+    } catch (__) {
+    }
+
+    return ''; // use external default escaping
+  }
+}).use(require('markdown-it-anchor'), {})
+  .use(require('markdown-it-toc'));
+
+var expressMarkdown = function (options) {
+  var dir = path.resolve(options.directory);
+
+  return function (req, res, next) {
+    var file = req.url.toString().toLowerCase();
+
+    var test = file.match(/^(.*)\.md$/);
+    if(test) {
+      file = test[1];
+    }
+    if (file === '/') {
+      file = 'index';
+    }
+
+    file = dir + '/' + url.parse(file).pathname;
+    file = path.resolve(file) + '.md';
+
+    if (file.substr(0, dir.length) !== dir) {
+      return res.send(400);
+    }
+
+    fs.exists(file, function (exists) {
+      if (!exists)
+        return next();
+
+      fs.readFile(file, 'utf8', function (err, data) {
+        if (err) {
+          return next(err);
+        }
+        res.render('markdown-show', {markdown: md.render(data)});
+      });
+    });
+  }
+};
+
+var config = fs.existsSync(__dirname + '/config.json') ? require('./config.json') : {};
+
+config = _.extend({
+  directory: __dirname + '/samples'
+}, config);
 
 var app = express();
 app.set('view engine', 'jade');
 app.use('/bower_components', express.static(__dirname + '/bower_components'));
-
-app.get('/', function (req, res) {
-  res.render('index', { title: 'Hey!' });
-});
-
-var port = 5000;
+app.use(expressMarkdown(config));
+var port = config.port || 5000;
 app.listen(port);
 console.log('App listening on port ' + port) + '.';
